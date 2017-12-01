97501c212b1a41ab741417d8b6358afe18394677
Pötyi <tpeti955@gmail.com>
2017-12-02 00:38:10 +0100

    Add project files.

---
+++ bot/main.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bot
+{
+    class main
+    {
+        static void Main(string[] args)
+        => new main().Start_async().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+
+        public async Task Start_async()
+        {
+            _client = new DiscordSocketClient();
+
+            string token= "Mzg1NTU5MDIwMjIxNzU5NDkw.DQM6nA.odxmUV2q08a5df0vhgJorCp3GIQ";
+
+            await _client.LoginAsync(TokenType.Bot, token); 
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+            _handler = new CommandHandler();
+
+            await _handler.InitializeAsync(_client);
+
+        }
+
+
+    }
+       
+}
+
