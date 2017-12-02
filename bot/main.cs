86262f13027f476ff2a001232a31d893bcce5e95
Hermit <hermit997@gmail.com>
2017-12-02 15:17:53 +0100

    fg

--- bot/main.cs
+++ bot/main.cs
@@ -20,13 +20,22 @@ namespace bot
 
         public async Task Start_async()
         {
-            _client = new DiscordSocketClient();
+            //_client = new DiscordSocketClient();
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            _client.Log += Log;
 
             string token= "Mzg1NTU5MDIwMjIxNzU5NDkw.DQM6nA.odxmUV2q08a5df0vhgJorCp3GIQ";
 
             await _client.LoginAsync(TokenType.Bot, token);
             
             await _client.StartAsync();
+
+           
 
             _handler = new CommandHandler();
 
@@ -35,6 +44,11 @@ namespace bot
             await Task.Delay(-1);//ezután semmi nem fut le
         }
 
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
 
     }
        
